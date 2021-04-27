1c73abef1863fbe888d3aa156870fb25a9da3bd1
Chris Camel <camel.christophe@gmail.com>
2021-04-27 16:49:20 +0200

    :white_check_mark: add tests for new source func
    Signed-off-by: Chris Camel <camel.christophe@gmail.com>

---
+++ server/source/source_test.go
@@ -0,0 +1,62 @@
+package source
+
+import (
+	"context"
+	"net/url"
+	"testing"
+
+	"github.com/reactivex/rxgo/v2"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+type dumbSource struct {
+	uri *url.URL
+}
+
+func (s dumbSource) URI() *url.URL {
+	return s.uri
+}
+
+func (s dumbSource) NewConsumer(_ context.Context, _ string, _ int64) rxgo.Observable {
+	return nil
+}
+
+func TestNew(t *testing.T) {
+	Convey("Considering a set of source factories with schemes 'foo' and 'bar'", t, func(c C) {
+		RegisterFactory("foo", func(uri *url.URL) (Source, error) {
+			return dumbSource{uri: uri}, nil
+		})
+		RegisterFactory("bar", func(uri *url.URL) (Source, error) {
+			return dumbSource{uri: uri}, nil
+		})
+		So(len(sourceFactories), ShouldEqual, 2)
+
+		Convey("When calling New() with scheme 'foo'", func(c C) {
+			url, err := url.Parse("foo://xyz?t=0")
+			So(err, ShouldBeNil)
+
+			source, err := New(url)
+
+			Convey("Then source is found", func(c C) {
+				So(err, ShouldBeNil)
+				So(source, ShouldNotBeNil)
+
+				So(source.URI(), ShouldEqual, url)
+			})
+		})
+
+		Convey("When calling New() with scheme 'barbar'", func(c C) {
+			url, err := url.Parse("barbar://xyz?t=0")
+			So(err, ShouldBeNil)
+
+			source, err := New(url)
+
+			Convey("Then source is not found", func(c C) {
+				So(err, ShouldNotBeNil)
+				So(source, ShouldBeNil)
+
+				So(err, ShouldBeError, "scheme barbar is not supported (available are: foo,bar): incorrect scheme")
+			})
+		})
+	})
+}
