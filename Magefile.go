38f377fefe0b2e48a22a6a9d7028b2ad26bd7754
ccamel <camel.christophe@gmail.com>
2023-08-17 19:42:18 +0200

    :construction_worker: add installTools target

--- Magefile.go
+++ Magefile.go
@@ -4,41 +4,35 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
 	"os/exec"
 
-	"github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
+	"github.com/fatih/color"
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
 )
 
-// Default target to run when none is specified
-// If not set, running mage will list available targets
-// var Default = Build
+var (
+	magenta = color.New(color.FgMagenta).SprintFunc()
+	cyan    = color.New(color.FgCyan).SprintFunc()
+	yellow  = color.New(color.FgYellow).SprintFunc()
+	green   = color.New(color.FgGreen).SprintFunc()
+	red     = color.New(color.FgRed).SprintFunc()
+)
 
-// A build step that requires additional params, or platform specific steps for example
-func Build() error {
-	mg.Deps(InstallDeps)
-	fmt.Println("Building...")
-	cmd := exec.Command("go", "build", "-o", "MyApp", ".")
-	return cmd.Run()
+func installTools() {
+	mg.Deps(mg.F(installGoTool, "gofumpt", "mvdan.cc/gofumpt", "v0.5.0"))
+	mg.Deps(mg.F(installGoTool, "gothanks", "psampaz/gothanks", "latest"))
+	mg.Deps(mg.F(installGoTool, "goconvey", "smartystreets/goconvey", "latest"))
+	mg.Deps(mg.F(installGoTool, "golangci-lint", "github.com/golangci/golangci-lint/cmd/golangci-lint", "v1.54.1"))
 }
 
-// A custom install step if you need your bin someplace other than go/bin
-func Install() error {
-	mg.Deps(Build)
-	fmt.Println("Installing...")
-	return os.Rename("./MyApp", "/usr/bin/MyApp")
-}
+func installGoTool(name, p, version string) error {
+	if _, err := exec.LookPath(name); err == nil {
+		return nil
+	}
 
-// Manage your deps, or running package managers.
-func InstallDeps() error {
-	fmt.Println("Installing Deps...")
-	cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
-	return cmd.Run()
-}
-
-// Clean up after yourself
-func Clean() {
-	fmt.Println("Cleaning...")
-	os.RemoveAll("MyApp")
+	fmt.Println("🚚", cyan("installing"), green(name), magenta(version))
+	return sh.Run("go", "install", fmt.Sprintf("%s@%s", p, version))
 }
